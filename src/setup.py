a0330b33b95a2069e4de126e9d9a9f72afddbf18
Said van de Klundert <said.van.de.klundert@ibm.com>
2022-01-04 21:16:24 +0100

    changes

--- src/setup.py
+++ src/setup.py
@@ -4,4 +4,13 @@ setup(
     name="example",
     version="0.1",
     packages=["example"],
+    description="This is an example package that can serve as a template.",
+    url="https://github.com/saidvandeklundert/python_template",
+    classifiers=["Programming Language :: Python :: 3"],
+    python_requires=">=3.8, <4",
+    project_urls={
+        "Bug Reports": "https://github.com/saidvandeklundert/python_template/issues",
+        "Source": "https://github.com/saidvandeklundert/python_template",
+        "Documentation": "https://github.com/saidvandeklundert/python_template/wiki",
+    },
 )
